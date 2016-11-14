54dbec51639d82dfb390ecddeadb1e187a2c2758
201601984 <wntjq3247@naver.com>
2016-11-14 22:26:00 +0900

    first commit

---
+++ 그린풋팀/Turtle.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class t here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Turtle extends Lobster //implements TurtleInterface, Freezable
+{
+    public void act() 
+    {
+        
+    }
+}
